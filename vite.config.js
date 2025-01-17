11e320785fe216f12019a104a547ef1b6a942dd5
Ryan Ortiz <uconnmemories@gmail.com>
2025-01-16 21:50:18 -0500

    I changed everything so it could become a website

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+// Export the Vite configuration
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // You can change this to any available port
+  },
+  publicDir: 'public', // Directory for static assets
+  root: path.resolve(__dirname), // Ensure Vite considers the root directory
+  build: {
+    outDir: path.resolve('./dist'), // Output directory for the build
+  },
+  esbuild: {
+    loader: {
+      '.js': 'jsx', // Tells Vite to treat .js files as JSX
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Allows imports like '@/some/path'
+    },
+  },
+  base: './', // Ensures assets are referenced correctly in production
+});
