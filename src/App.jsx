50ed691dc8c8ec54372bbb49daea57c6acf6b16d
Ryan Ortiz <uconnmemories@gmail.com>
2025-01-16 22:17:47 -0500

    Fixing video path typo

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
       {/* First video control */}
       <VideoControl
-        videoUrl="typefaster.mp4" // Music Video File
+        videoUrl="/typefaster.mp4" // Music Video File
         buttonColor="#31602b" // Button color
         buttonHoverColor="#3d8234" // Hover color for button
         backgroundColor="#cdd745" // Background color when not playing
@@ -34,7 +34,7 @@ function App() {
 
       {/* Second video control */}
       <VideoControl
-        videoUrl="dolphon.mp4"  // Music Video File
+        videoUrl="/dolphon.mp4"  // Music Video File
         buttonColor="#fdeb8e" // Button color (e.g., tomato color)
         buttonHoverColor="#fdea00" // Hover color (e.g., darker tomato)
         backgroundColor="#cdd745" // Default background color
